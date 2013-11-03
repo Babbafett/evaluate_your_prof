304e749190d0987f21963c7e3f71f82f41eb1be1
Babbafett <Babbafett@hotmail.de>
2013-11-03 14:17:05 +0100

    DB Connection Export

--- php/getConnection.php
+++ php/getConnection.php
@@ -1,5 +1,5 @@
 <?php
-$connect = new mysqli('localhost', 'root', '6g33SeYtEX', 'db_evaluate_your_prof');
+$connect = new mysqli('localhost', 'root', '', 'db_evaluate_your_prof');
 if ($connect -> connect_errno) {
 	echo "Failed to connect to MySQL: " . $connect -> connect_error;
 }
