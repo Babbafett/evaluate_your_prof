4dde73ecd5e5a4c175af91b41954d3598bf5c75a
Babbafett <Babbafett@hotmail.de>
2014-02-20 17:34:09 +0100

    Commit

--- php/getConnection.php
+++ php/getConnection.php
@@ -1,5 +1,5 @@
 <?php
-$connect = new mysqli('localhost', 'root', '', 'db_evaluate_your_prof');
+$connect = new mysqli('localhost', 'root', 's', 'db_evaluate_your_prof');
 if ($connect -> connect_errno) {
 	echo "Failed to connect to MySQL: " . $connect -> connect_error;
 }
